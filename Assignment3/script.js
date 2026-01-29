a8351ed40b9ab4dfc641688b8841ea2d5211d6a5
XiangyuTong1 <tong.xian@northeastern.edu>
2026-01-28 19:14:24 -0500

    Implement page load initialization and dropdown toggle functionality

--- Assignment3/script.js
+++ Assignment3/script.js
@@ -1,18 +1,56 @@
-//Title constructor function that creates a Title object
-function Title(t1) 
-{ this.mytitle = t1;
+function Title(t1) { 
+    this.mytitle = t1;
 }
 
-Title.prototype.getName = function () 
-{ 
-return (this.mytitle);
+Title.prototype.getName = function () { 
+    return (this.mytitle);
 }
 
 var socialMedia = {
-  facebook : 'http://facebook.com',
-  twitter: 'http://twitter.com',
-  flickr: 'http://flickr.com',
-  youtube: 'http://youtube.com'
+    facebook: 'http://facebook.com',
+    twitter: 'http://twitter.com',
+    flickr: 'http://flickr.com',
+    youtube: 'http://youtube.com'
 };
 
 var t = new Title("CONNECT WITH ME!");
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('studentInfo').innerHTML = 'Your Full Name - NUID: 123456789';
+    
+    var dropdowns = document.querySelectorAll('.dropDownTextArea');
+    dropdowns.forEach(function(dropdown) {
+        dropdown.style.display = 'none';
+    });
+    
+    var submitBtn = document.getElementById('button');
+    submitBtn.disabled = true;
+    
+    attachEventListeners();
+});
+
+function attachEventListeners() {
+    var checkboxes = document.querySelectorAll('#myTable tbody tr:not(.dropDownTextArea) input[type="checkbox"]');
+    checkboxes.forEach(function(checkbox) {
+        checkbox.addEventListener('change', handleCheckboxChange);
+    });
+    
+    var arrows = document.querySelectorAll('.arrow');
+    arrows.forEach(function(arrow) {
+        arrow.addEventListener('click', toggleDropdown);
+    });
+}
+
+function toggleDropdown(e) {
+    var arrow = e.target;
+    var currentRow = arrow.closest('tr');
+    var dropdownRow = currentRow.nextElementSibling;
+    
+    if(dropdownRow && dropdownRow.classList.contains('dropDownTextArea')) {
+        if(dropdownRow.style.display === 'none' || dropdownRow.style.display === '') {
+            dropdownRow.style.display = 'table-row';
+        } else {
+            dropdownRow.style.display = 'none';
+        }
+    }
+}
